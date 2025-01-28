3d3b9740e31c7bd7f9b9c03650bd00642dc29212
SilvaProDev <ouattara.daouda4613@gmail.com>
2025-01-28 16:50:29 +0000

    découpage du template

---
+++ front-end/src/layout/TheContent.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <TheMain/>
+        <TheFooter/>
+    </div>
+</template>
+<script lang="ts" setup >
+    import TheFooter from "./TheFooter.vue";
+    import TheMain from "./TheMain.vue";
+</script>
